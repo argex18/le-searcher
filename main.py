b3a6982fd1a884c6fe24e2d939bfd89352253754
GIOCHI <alessandro.argex@gmail.com>
2020-12-21 19:04:04 +0100

    Check of main.py parameters added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from traceback import print_exc
 
 audio_file = None
 content_type = None
+json_file = None
+script_name = sys.argv.pop(0)
 
 try:
     if len(sys.argv) < 2:
@@ -16,10 +18,26 @@ try:
         json_file = sys.argv[2]
 except getopt.GetoptError:
     print_exc()
-    """ i = 3
-    while i < len(sys.argv):
-        print(f'{sys.argv[i]} not recognized')
-        i += 1 """
+    if len(sys.argv) < 2:
+        i = 0
+        while i < 2:
+            try:
+                print(f'par{i}: {sys.argv[i]}')
+                i += 1
+            except Exception:
+                break
+        if i == 0:
+            print('error...audio_file not specified')
+            print('error...content_type not specified')
+        else:
+            print('error...content type not specified')
+    else:
+        i = 3
+        while i < len(sys.argv):
+            print(f'"{sys.argv[i]}" not recognized')
+            i += 1
+    sys.exit(1)
+
 
 if __name__ == '__main__':
-    data = le_searcher.convert(audio_file, content_type)
+    data = le_searcher.convert(audio_file, content_type, json_file)
